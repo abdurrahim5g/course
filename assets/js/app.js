54efeb905526e55c56dd4286b988877f5579b71b
f-rahim <freelancerrahim26@gmail.com>
2021-12-24 01:45:18 +0600

    Testmonial page done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,25 @@
 $(document).ready(function () {
   // Mixit Up js
   var mixer = mixitup(".project-filter");
+
+  // testmonial slider
+  $("#testmonial-slider").slick({
+    slidesToShow: 1,
+    centerMode: true,
+    centerPadding: "20%",
+    infinite: true,
+    // cssEase: "linear",
+    speed: 500,
+    prevArrow: "<img src='../assets/images/arrow-left.png'>",
+    nextArrow:
+      "<img src='../assets/images/arrow-right.png' class='arrow-right'>",
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          centerPadding: "10%",
+        },
+      },
+    ],
+  });
 });
